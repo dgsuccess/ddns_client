3816f4199a7c9d934caf4134aaec1d84af5aac33
chenggong <chenggong@tianyujingwei.com>
2018-06-22 16:58:42 +0800

    aliyun ddns client for linux/macox

---
+++ aliyun-ddns-client/common/GetConfig.go
@@ -0,0 +1,89 @@
+/**
+ * @Desc
+ * @author zjhfyq 
+ * @data 2018/4/26 14:13.
+ */
+package common
+
+import (
+	"aliyun-ddns-client/common/goconfig"
+	"log"
+	"strconv"
+	"aliyun-ddns-client/model"
+)
+
+//必要的配置参数
+var AccessKeyId string
+var AccessKeySecret string
+var DomainName string
+
+//可选的参数
+var CycleTime int
+var RecordConfig model.Record
+
+
+func init()  {
+	cf , err := goconfig.LoadConfigFile("./ddns.conf")
+	if err != nil {
+		panic("没有找到./ddns.conf的配置文件")
+	}else {
+		must,err  :=cf.GetSection("Must")
+		if err != nil {
+			panic("没有AccessKey相关配置")
+		}else {
+			if value , ok :=  must["AccessKeyId"];ok {
+				AccessKeyId=value
+				//log.Println(AccessKeyId)
+			}else {
+				panic("没有AccessKeyId相关配置")
+			}
+			if value , ok := must["AccessKeySecret"];ok {
+				AccessKeySecret = value
+				//log.Println(AccessKeySecret)
+			}else {
+				panic("没有AccessKeySecret相关配置")
+			}
+			if value ,ok := must["DomainName"];ok {
+				DomainName = value
+				//log.Println(DomainName)
+			}else {
+				panic("Local下面缺少DomainName的配置")
+			}
+		}
+
+		paramMap , err := cf.GetSection("Optional")
+		if err != nil {
+			log.Println("没有Param相关配置,将采用原始配置信息")
+		}else {
+			if value , ok := paramMap["RecordId"];ok {
+				RecordConfig.RecordId = value;
+			}
+			if value , ok := paramMap["RR"];ok {
+				RecordConfig.RR = value;
+			}
+			if value , ok := paramMap["Type"];ok {
+				RecordConfig.Type = value;
+			}
+			if value , ok := paramMap["Value"];ok {
+				RecordConfig.Value = value;
+			}
+
+			if value , ok := paramMap["TTL"];ok {
+				RecordConfig.TTL= value;
+			}
+			if value , ok := paramMap["Priority"];ok {
+				RecordConfig.Priority = value;
+			}
+			if value , ok := paramMap["Line"];ok {
+				RecordConfig.Line = value;
+			}
+			if value ,ok := paramMap["CycleTime"];ok {
+				CycleTime, err  = strconv.Atoi(value)
+				if err != nil {
+					log.Println(err)
+					CycleTime = 300
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-ddns-client/main.go
@@ -0,0 +1,142 @@
+/**
+ * @Desc
+ * @author zjhfyq 
+ * @data 2018/4/26 13:44.
+ */
+package main
+
+import (
+	"os"
+	"log"
+	"aliyun-ddns-client/common"
+	"aliyun-ddns-client/httpopt"
+	"aliyun-ddns-client/util"
+	"aliyun-ddns-client/model"
+	"time"
+)
+
+
+var records []model.Record
+
+var lastIp =""
+var nowIp = ""
+var dnsIp = ""
+
+func main() {
+	if len(os.Args)<=1 {
+		common.PrintHelp()
+		return
+	}
+	args := os.Args[1:]
+	//获取当前出口IP
+	nowIp = util.GetLocalIp()
+	lastIp = nowIp
+	log.Println("当前出口IP：",nowIp)
+	//获取解析
+	url := common.GetUrlAfterSignature(common.API_STRING,common.AccessKeySecret,common.GetDescribeDomainRecordsParams())
+	//log.Println("url:",url)
+	records = httpopt.GetRecordDesc(url)
+	//log.Println(len(records))
+	if len(records)==0 {
+		log.Println("未能获取到解析记录，检查网络")
+	}else {
+		if len(records)>0 {
+			for _,record := range records {
+				if record.Type  =="A" {
+					lastIp = record.Value
+					log.Println("域名"+common.DomainName+"对应IP：",lastIp)
+					break
+				}
+			}
+		}
+	}
+	start := false
+	for _ , arg := range args {
+		if arg == "-ls" {
+			if records != nil {
+				common.PrintRecords(records)
+				//getDnsInfo()
+			}else {
+				log.Println("未能获取到解析记录，检查网络")
+			}
+
+		}
+		if arg =="--help" || arg == "-help" || arg == "--h" || arg =="-h" {
+			common.PrintHelp()
+		}
+		if arg == "-start" {
+			start = true
+		}
+	}
+	if start {
+		log.Println("DDNS已经启动...")
+		startDDNS()
+	}
+}
+
+func startDDNS()  {
+	log.Println("DDNS已经启动...")
+	for {
+		getDnsInfo()
+		nowIp = util.GetLocalIp()
+		if nowIp != lastIp {
+			log.Println("主机IP:",nowIp,"发生变化，上次IP：",lastIp,"，修改域名解析记录")
+			updateDomainRecord()
+			lastIp = nowIp
+		}else if nowIp != dnsIp {
+			log.Println("主机IP:",nowIp,"和解析记录IP:",dnsIp,"不一致，修改域名解析记录")
+			updateDomainRecord()
+		}else {
+			log.Println("主机出口IP没有发生变化并且和DNS解析保持一致，不需要修改解析记录")
+		}
+		//睡眠
+		time.Sleep(time.Duration(int64(common.CycleTime))*time.Second)
+	}
+}
+
+func updateDomainRecord()  {
+	if records == nil {
+		url := common.GetUrlAfterSignature(common.API_STRING,common.AccessKeySecret,common.GetDescribeDomainRecordsParams())
+		records = httpopt.GetRecordDesc(url)
+	}
+	if records != nil {
+		for _,record := range records {
+			if record.RR == "pd"{
+				params := common.GetUpdateDomainRecordParams()
+				params["RecordId"]= record.RecordId
+				params["RR"]= record.RR
+				params["Type"]= record.Type
+				params["Value"]= nowIp
+				url := common.GetUrlAfterSignature(common.API_STRING,common.AccessKeySecret,params)
+				result := httpopt.UpdateRecord(url)
+				log.Println("主机记录RR：",record.RR,"的修改结果：",result)
+				break
+			}
+		}
+	}else {
+		log.Println("获取解析失败，请检查网络环境")
+	}
+}
+
+
+
+
+func getDnsInfo()  {
+	url := common.GetUrlAfterSignature(common.API_STRING,common.AccessKeySecret,common.GetDescribeDomainRecordsParams())
+	records = httpopt.GetRecordDesc(url)
+	if len(records)>0 {
+		for _, record := range records {
+			if record.RR == "pd" {
+				dnsIp = record.Value
+				log.Println("pd."+common.DomainName+" 对应IP：",dnsIp)
+				break
+			}
+		}
+	}else {
+		log.Println("没有解析记录，DDNS不会生效，前往阿里云DNS添加相关解析")
+	}
+}
+
+
+
+
